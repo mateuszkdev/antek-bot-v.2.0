b345878c3ebad88321a6764555c6bca0cf899abc
Mateusz Kalaga <46076250+mateuszkdev@users.noreply.github.com>
2020-08-02 22:20:32 +0200

    Update config.js

--- src/database/config.js
+++ src/database/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    token: "NTQ0MTgwNDk3NTIyMDk4MTc3.Xj9AoA.V8bVA7WJ1-MGANOj8r6TOnKiNaI",
+    token: "heh",
     prefix: "a!",
     color: "#ffffff",
     owner: "395266229436153868",
